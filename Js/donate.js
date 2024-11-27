56f3cf91572e1b928a6151eb9a40fd09b4485975
MuhaimanAlamMazumder <muhaimennabil@gmail.com>
2024-11-27 21:10:01 +0600

    donation app using js

---
+++ Js/donate.js
@@ -0,0 +1,12 @@
+//add money to the donation
+
+document.getElementById('btn-donate-money').addEventListener('click', function(event){
+
+
+    event.preventDefault();
+
+    const addMoneyInput = document.getElementById('input-add-money').value;
+    console.log(addMoneyInput);
+
+  
+});
