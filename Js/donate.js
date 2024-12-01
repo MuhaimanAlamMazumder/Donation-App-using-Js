c4ca2f6fe54de88c43620c822e45fd876a98af77
MuhaimanAlamMazumder <muhaimennabil@gmail.com>
2024-12-01 18:00:10 +0600

    Donation option added using js

--- Js/donate.js
+++ Js/donate.js
@@ -4,9 +4,109 @@ document.getElementById('btn-donate-money').addEventListener('click', function(e
 
 
     event.preventDefault();
+    console.log('donation button clicked');
 
-    const addMoneyInput = document.getElementById('input-add-money').value;
+   
+
+    // const addMoneyInput = document.getElementById('input-add-money').value;
+    // console.log(addMoneyInput);
+
+    const addMoneyInput = getInputFieldValueById('input-add-money');
+    
     console.log(addMoneyInput);
+    
+
+
+    if(addMoneyInput >= 0){
+
+        const balance = getTextFieldValueById('account-balance');
+
+        const newBalance = balance + addMoneyInput;
+
+        document.getElementById('account-balance').innerText = newBalance;
+
+    
+
+    }
+    else{
+
+        alert('Failed to donate money.')
+    }
+
+
+  
+});
+
+
+
+document.getElementById('btn-donate-money2').addEventListener('click', function(event){
+
+
+    event.preventDefault();
+    console.log('donation button clicked');
+
+   
+
+    // const addMoneyInput = document.getElementById('input-add-money').value;
+    // console.log(addMoneyInput);
+
+    
+    const addMoneyInput2 = getInputFieldValueById2('input-add-money2');
+    
+    console.log(addMoneyInput2);
+
+
+    if(addMoneyInput2 >= 0){
+
+        const balance = getTextFieldValueById('account-balance2');
+
+        const newBalance = balance + addMoneyInput2;
+
+        document.getElementById('account-balance2').innerText = newBalance;
+
+
+
+    }
+    else{
+
+        alert('Failed to donate money.')
+    }
+
+
+  
+});
+
+document.getElementById('btn-donate-money3').addEventListener('click', function(event){
+
+
+    event.preventDefault();
+    console.log('donation button clicked');
+
+   
+
+    // const addMoneyInput = document.getElementById('input-add-money').value;
+    // console.log(addMoneyInput);
+
+    
+    const addMoneyInput3 = getInputFieldValueById3('input-add-money3');
+    
+    console.log(addMoneyInput3);
+
+
+    if(addMoneyInput3 >= 0){
+
+        const balance = getTextFieldValueById('account-balance3');
+
+        const newBalance = balance + addMoneyInput3;
+
+        document.getElementById('account-balance3').innerText = newBalance;
+
+    }
+    else{
+
+        alert('Failed to donate money.')
+    }
+
 
   
 });
---
+++ Js/utilities.js
@@ -0,0 +1,38 @@
+function getInputFieldValueById(id){
+
+    const inputValue = document.getElementById(id).value;
+    const inputNumber = parseFloat(inputValue);
+
+
+    return inputNumber;
+}
+
+
+function getTextFieldValueById(id){
+
+    const textValue = document.getElementById(id).innerText;
+    const textNumber = parseFloat(textValue);
+
+    return textNumber;
+}
+
+
+
+
+function getInputFieldValueById2(id){
+
+    const inputValue = document.getElementById(id).value;
+    const inputNumber = parseFloat(inputValue);
+
+    return inputNumber;
+}
+
+
+
+function getInputFieldValueById3(id){
+
+    const inputValue = document.getElementById(id).value;
+    const inputNumber = parseFloat(inputValue);
+
+    return inputNumber;
+}
